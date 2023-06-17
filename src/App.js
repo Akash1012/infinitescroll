1a5f06a17d3cd50654b303d20f72d8d390fc88f9
akash <akash@akashs-MacBook-Air.local>
2023-06-17 16:36:14 +0530

    add infinite scoll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+
+  const infiniteScroll = useRef(null);
+
+  useEffect(() => {
+    const getTheData = async () => {
+      const value = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+      );
+      const json = await value.json();
+      const { products, total } = json;
+
+      setData([...data, ...products]);
+      setTotalPage(total / 10);
+    };
+    getTheData();
+  }, [page]);
+
+  const onScroll = (e) => {
+    if (infiniteScroll.current) {
+      const { scrollTop, scrollHeight, clientHeight } = infiniteScroll.current;
+
+      if (scrollTop + clientHeight + 1 > scrollHeight) {
+        if (page < totalPage) {
+          setPage((page) => page + 1);
+        }
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        ref={infiniteScroll}
+        onScroll={() => onScroll()}
+        // style={{
+
+        // }}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          backgroundColor: "darkblue",
+          height: "800px",
+          overflow: "auto",
+        }}
+      >
+        {data.map((item) => {
+          const { id, title, thumbnail } = item;
+          return (
+            <div
+              key={id}
+              style={{
+                margin: "10px",
+                padding: "10px",
+                backgroundColor: "grey",
+              }}
+            >
+              <img
+                src={thumbnail}
+                alt={title}
+                style={{
+                  width: "300px",
+                  height: "290px",
+                  borderRadius: "10px",
+                }}
+              />
+              <p>{title}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
